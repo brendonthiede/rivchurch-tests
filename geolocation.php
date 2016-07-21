f536f6f9208167dc554b54518e54b7c763e21a88
brendonthiede <brendon.thiede@gmail.com>
2016-07-21 12:48:40 -0400

    Added geolocation error handling

--- geolocation.php
+++ geolocation.php
@@ -21,7 +21,7 @@
     function getLocation() {
         startDiv.innerHTML = new Date();
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
+            navigator.geolocation.getCurrentPosition(showPosition, showError);
         } else {
             x.innerHTML = "Geolocation is not supported by this browser.";
         }
@@ -60,6 +60,23 @@
             "<br><br>";
 
         endDiv.innerHTML = new Date();
+    }
+
+    function showError(error) {
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+                x.innerHTML = "User denied the request for Geolocation.";
+                break;
+            case error.POSITION_UNAVAILABLE:
+                x.innerHTML = "Location information is unavailable.";
+                break;
+            case error.TIMEOUT:
+                x.innerHTML = "The request to get user location timed out.";
+                break;
+            case error.UNKNOWN_ERROR:
+                x.innerHTML = "An unknown error occurred.";
+                break;
+        }
     }
 
     getLocation();
